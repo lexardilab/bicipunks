7a5502b2d1d12197e33f52dafe5c3264a1050476
lexardi <lexardi.lab@gmail.com>
2025-02-14 11:33:53 +0100

    index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 export default function Header() {
     return (
       <div className="flex justify-center items-center h-screen">
-        <Image src="/BicipunksWeb.svg" width="800" height="100" alt="Bicipunks"
+        <Image src="/logo.svg" width="800" height="100" alt="Bicipunks"
         
         
         />
